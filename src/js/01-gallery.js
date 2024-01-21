1d4ddd9185e6ff6e83a1cbaaa100a0d528e98a12
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-01-21 11:29:14 +0100

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from '../../node_modules/simplelightbox/dist/simple-lightbox.min';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const ulGallery = document.querySelector('.gallery');
+const liGallery = galleryItems
+  .map(
+    item =>
+      `<li><a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></a></li>`
+  )
+  .join('');
+ulGallery.innerHTML = liGallery;
+
+const lightBox = new SimpleLightbox('.gallery li a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
